d191abe487be8d264dfd9a10771e2b852bbd2832
reseadle37 <reseadle37@gmail.com>
2018-07-01 23:21:05 +0400

    change readme and webpack config

--- code/webpack.config.js
+++ code/webpack.config.js
@@ -5,87 +5,95 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+var customConfig = (function webpackConfig() {
+    var config = Object.assign({});
+
+    config.entry = './src/app.js';
+
+    config.output = {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    };
+
+    config.module = {
+      rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        },
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { 
+              loader: 'css-loader', 
+              options: { minimize: true } 
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                  plugins: [
+                      autoprefixer({
+                          browsers:['ie >= 8', 'last 4 version']
+                      })
+                  ],
+            }
+            }, 
+              'sass-loader']
+            })
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [{
+              loader: 'file-loader',
+              options: {
+                  name: '[name].[ext]',
+                  outputPath: 'fonts/'
+              }
+          }]
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                  name: '[name].[ext]',
+                  outputPath: 'icons/'
+              }
+            }
+          ]
         }
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [
-          { 
-            loader: 'css-loader', 
-            options: { minimize: true } 
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-                plugins: [
-                    autoprefixer({
-                        browsers:['ie >= 8', 'last 4 version']
-                    })
-                ],
-          }
-          }, 
-            'sass-loader']
-          })
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-            loader: 'file-loader',
-            options: {
-                name: '[name].[ext]',
-                outputPath: 'fonts/'
-            }
-        }]
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-                name: '[name].[ext]',
-                outputPath: 'icons/'
-            }
-          }
-        ]
-      }
-    ]
-  },
-    plugins: [
-        new ExtractTextPlugin('style.css'),
-        new HtmlWebpackPlugin({
-            inject: false,
-            hash: true,
-            template: './src/index.html',
-            filename: 'index.html'
-          })
-        // new CopyWebpackPlugin(
-        //   [
-        //     {
-        //       from: path.resolve(__dirname, 'assets'),
-        //       to: path.resolve(__dirname, 'dist/assets/')
-        //     },
-        //   ]),
-        // new ImageminPlugin({
-        //   test: /\.(jpe?g|png|gif|svg)$/i,
-        //   pngquant: {
-        //     quality: '95-100'
-        //   }
-        // })
-    ]
-};
+      ]
+    };
+    config.plugins = [
+      new ExtractTextPlugin('style.css'),
+      new HtmlWebpackPlugin({
+          inject: false,
+          hash: true,
+          template: './src/index.html',
+          filename: 'index.html'
+        }),
+      // new CopyWebpackPlugin(
+      //   [
+      //     {
+      //       from: path.resolve(__dirname, 'assets'),
+      //       to: path.resolve(__dirname, 'dist/assets/')
+      //     },
+      //   ]),
+      // new ImageminPlugin({
+      //   test: /\.(jpe?g|png|gif|svg)$/i,
+      //   pngquant: {
+      //     quality: '95-100'
+      //   }
+      // })
+    ];
+
+    return config;
+});
+
+module.exports = customConfig;
